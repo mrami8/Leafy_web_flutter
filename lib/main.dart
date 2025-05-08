aae906cf6f86a11eb440f3ce3f51aad044cfca6f
CarlosCode777 <casico23@bemen3.cat>
2025-05-08 19:52:42 +0200

    RenameFolders

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,13 @@ import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'supabase_config.dart'; // Archivo donde se configura Supabase
-import 'screens/LoginScreen.dart'; // Pantalla de inicio de sesión
-import 'screens/RegisterScreen.dart'; // Pantalla de registro
-import 'screens/PlantsScreen/search_screen.dart'; // Pantalla de búsqueda de plantas
+import 'screens/LoginScreen/LoginScreen.dart'; // Pantalla de inicio de sesión
+import 'screens/LoginScreen/RegisterScreen.dart'; // Pantalla de registro
+import 'screens/SearchScreen/search_screen.dart'; // Pantalla de búsqueda de plantas
 import 'screens/CalendarScreen/calendar_screen.dart'; // Pantalla del calendario
 import 'screens/ProfileScreen/profile_screen.dart'; // Pantalla del perfil
 import 'providers/General/auth_provider.dart'; // Provider de autenticación
-import 'providers/Plants/plant_search_provider.dart'; // Provider para buscar plantas
+import 'providers/Search/plant_search_provider.dart'; // Provider para buscar plantas
 import 'providers/Calendar/notification_provider.dart'; // Provider para notificaciones del calendario
 import 'providers/Profile/user_profile_provider.dart'; // Provider del perfil de usuario
 import 'providers/Garden/progress_provider.dart'; // Provider de progreso del jardín
